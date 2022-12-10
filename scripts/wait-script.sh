af90a5b4ef9adca936f05a852e1f3308f464fb83
Griffire <eaglegriff@mail.ru>
2022-12-10 18:19:02 +0300

    docker compose -> docker-compose

--- scripts/wait-script.sh
+++ scripts/wait-script.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
+echo "hello"
+
 IFS="," read -ra PORTS <<<"$WAIT_PORTS"
 path=$(dirname "$0")
 
+echo "hello2"
 PIDs=()
 for port in "${PORTS[@]}"; do
+    echo "for1"
   "$path"/wait-for.sh -t 120 "http://localhost:$port/manage/health" -- echo "Host localhost:$port is active" &
   PIDs+=($!)
 done
 
+echo "hello3"
 for pid in "${PIDs[@]}"; do
   if ! wait "${pid}"; then
     exit 1
   fi
 done
+echo "hello4"
